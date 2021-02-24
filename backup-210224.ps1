78b9e31c172528ec59bfe032b2323f10a6f57332
Jiapei Zeng <448311217@qq.com>
2021-02-24 22:39:02 +0800

    backup

---
+++ backup-210224.ps1
@@ -0,0 +1,163 @@
+try{if(Get-Command scoop){}} catch {iex (new-object net.webclient).downloadstring('https://get.scoop.sh')}
+scoop install git
+scoop bucket add Ash258 https://github.com/Ash258/Scoop-Ash258.git
+scoop bucket add bear https://github.com/AStupidBear/scoop-bear
+scoop bucket add comp500 https://github.com/comp500/scoop-comp500/
+scoop bucket add dorado https://github.com/h404bi/dorado
+scoop bucket add echo https://github.com/echoiron/echo-scoop.git
+scoop bucket add emulators https://github.com/hermanjustnu/scoop-emulators.git
+scoop bucket add enk https://github.com/pfmoore/scoop-enk
+scoop bucket add extras
+scoop bucket add galaxy-integrations https://github.com/borger/scoop-galaxy-integrations.git
+scoop bucket add games
+scoop bucket add java
+scoop bucket add jetbrains
+scoop bucket add jfut https://github.com/jfut/scoop-jfut.git
+scoop bucket add knox-scoop https://git.irs.sh/KNOXDEV/knox-scoop
+scoop bucket add lemon https://github.com/hoilc/scoop-lemon
+scoop bucket add main
+scoop bucket add maman https://github.com/maman/scoop-bucket
+scoop bucket add misc https://github.com/ondr3j/scoop-misc.git
+scoop bucket add mochi https://hub.fastgit.org/Qv2ray/mochi
+scoop bucket add nerd-fonts
+scoop bucket add nirsoft
+scoop bucket add nirsoft-alternative https://github.com/MCOfficer/scoop-nirsoft.git
+scoop bucket add nonportable
+scoop bucket add nsis https://github.com/NSIS-Dev/scoop-nsis
+scoop bucket add pleiades https://github.com/jfut/scoop-pleiades.git
+scoop bucket add portable https://github.com/nickbudi/scoop-bucket
+scoop bucket add python https://github.com/TheRandomLabs/Scoop-Python.git
+scoop bucket add r-bucket https://github.com/cderv/r-bucket.git
+scoop bucket add raisercostin 
+scoop bucket add rasa https://github.com/rasa/scoops.git
+scoop bucket add retools https://github.com/TheCjw/scoop-retools.git
+scoop bucket add scoop-clojure https://github.com/littleli/scoop-clojure.git
+scoop bucket add scoop-completion https://github.com/Moeologist/scoop-completion.git
+scoop bucket add scoopbucket https://github.com/yuanying1199/scoopbucket.git
+scoop bucket add scoopet https://github.com/integzz/scoopet
+scoop bucket add secrethub https://github.com/secrethub/scoop-secrethub
+scoop bucket add spotify https://github.com/TheRandomLabs/Scoop-Spotify.git
+scoop bucket add sushi https://github.com/kidonng/sushi
+scoop bucket add Sysinternals https://github.com/Ash258/Scoop-Sysinternals.git
+scoop bucket add TheRandomLabs 
+scoop bucket add tomato https://github.com/zhoujin7/tomato.git
+scoop bucket add user https://github.com/excitoon/scoop-user.git
+scoop bucket add versions
+scoop bucket add wangzq https://github.com/wangzq/scoop-bucket
+scoop bucket add wsl https://git.irs.sh/KNOXDEV/wsl
+scoop bucket add yubico-tool https://github.com/82p/scoop-yubico-bucket.git
+scoop install 7Zip
+scoop install AccessEnum
+scoop install AdExplorer
+scoop install apache
+scoop install apache-tinkerpop-gremlin-console
+scoop install apache-tinkerpop-gremlin-server
+scoop install aria2
+scoop install AutoHotKey
+scoop install bulk-crap-uninstaller
+scoop install cmake
+scoop install cmder
+scoop install codetrack
+scoop install crow-translate
+scoop install curl
+scoop install dark
+scoop install eartrumpet
+scoop install elixir
+scoop install emule-community
+scoop install erlang
+scoop install everything
+scoop install ffmpeg
+scoop install ffmpeg-shared
+scoop install fiddler
+scoop install Firacode
+scoop install flux
+scoop install geekuninstaller
+scoop install gettext
+scoop install gh
+scoop install GitKraken
+scoop install gnupg
+scoop install go
+scoop install gomake
+scoop install gpg4win
+scoop install graalvm
+scoop install gtools
+scoop install hub
+scoop install idm
+scoop install innounp
+scoop install insomnia
+scoop install insomnia-designer
+scoop install InternetHistoryBrowser
+scoop install ipfilter-updater
+scoop install kotlin
+scoop install lessmsi
+scoop install linqpad
+scoop install liteide
+scoop install llvm
+scoop install make
+scoop install markdown-monster
+scoop install md5
+scoop install mediainfo
+scoop install mkcert
+scoop install mouseinc
+scoop install msys2
+scoop install naiveproxy
+scoop install netch
+scoop install nimbleText
+scoop install nirlauncher
+scoop install nmap-portable
+scoop install notepadplusplus
+scoop install nuclear
+scoop install nuget
+scoop install nuget-package-explorer
+scoop install oh-my-posh3
+scoop install openjdk
+scoop install openssh-win32
+scoop install openssl
+scoop install openvpn-w7
+scoop install paint.net
+scoop install paket
+scoop install pkg-config
+scoop install PortMon
+scoop install ProcessExplorer
+scoop install ProcessMonitor
+scoop install qbittorrent
+scoop install qtranslate
+scoop install quicklook
+scoop install qv2ray
+scoop install racket
+scoop install RAMMap
+scoop install rbtray
+scoop install rpcs3
+scoop install rustup
+scoop install scoop-backup
+scoop install securitysoftview
+scoop install shellexview
+scoop install signal
+scoop install smartsvn
+scoop install snipaste
+scoop install snipaste-beta
+scoop install solar-putty
+scoop install SpaceSniffer
+scoop install speedtest-cli
+scoop install starship
+scoop install sudo
+scoop install teracopy-np
+scoop install thunderbird
+scoop install v2ray
+scoop install v2rayn
+scoop install Victor-Mono
+scoop install visual-arm-emulator
+scoop install vscode
+scoop install vt-cli
+scoop install win32-openssh
+scoop install winmerge
+scoop install winscp
+scoop install wireshark
+scoop install xray
+scoop install yori
+scoop install youtube-dl
+scoop install youtube-dl-wpf
+scoop install yubikey-personalization-tool
+scoop install zlib
+scoop install zulu10
+
